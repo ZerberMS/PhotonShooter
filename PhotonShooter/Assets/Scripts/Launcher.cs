cb992c299b6ed16255917396bdc4d7d12f3b524a
Michael Slavitsky <Slavitsky321@gmail.com>
2022-06-01 02:08:03 +0300

    Some coding stuff.
    Made a "Scripts" folder and a "Launcher" script, as well as starting the work on the UI element. (The first video.)

---
+++ PhotonShooter/Assets/Scripts/Launcher.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        Debug.Log("Connecting to master...");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined lobby");
+    }
+
+    void Update()
+    {
+        
+    }
+}
