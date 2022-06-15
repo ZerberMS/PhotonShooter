71520ec569f29db563e02278d6c6f2cbb6d790b4
benpanski <benpanski@gmail.com>
2022-06-15 14:01:58 +0300

    Before VIDEO 6 after deleting photon demo folder

--- PhotonShooter/Assets/Scripts/Launcher.cs
+++ PhotonShooter/Assets/Scripts/Launcher.cs
@@ -69,6 +69,11 @@ public class Launcher : MonoBehaviourPunCallbacks
 
         Player[] players = PhotonNetwork.PlayerList;
 
+        foreach (Transform Child in playerListContent)
+        {
+            Destroy(Child.gameObject);
+        }
+
         for (int i = 0; i < players.Count(); i++)
         {
             Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
@@ -109,6 +114,8 @@ public class Launcher : MonoBehaviourPunCallbacks
 
         for (int i = 0; i < roomList.Count; i++)
         {
+            if (roomList[i].RemovedFromList)
+                continue;
             Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
         }
     }
